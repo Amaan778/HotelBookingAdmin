5ca90cf03decafc9b5d2f68dad27ce6adf78e450
Amaan Ansari <amaanansari778@gmail.com>
2025-03-19 04:49:12 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/addhotel/HotelDashboard.kt
+++ app/src/main/java/com/app/hotelbookingadmin/addhotel/HotelDashboard.kt
@@ -5,9 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.app.hotelbookingadmin.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HotelDashboard : AppCompatActivity() {
+    private lateinit var btn:FloatingActionButton
+    private lateinit var recycler:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel_dashboard)
