0fc4bcd5c0fcede60f5b061737fe0dce5b18843e
Amaan Ansari <amaanansari778@gmail.com>
2025-03-23 09:31:31 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/addhotel/HotelRecycler.kt
+++ app/src/main/java/com/app/hotelbookingadmin/addhotel/HotelRecycler.kt
@@ -1,17 +1,25 @@
 package com.app.hotelbookingadmin.addhotel
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.app.hotelbookingadmin.R
 
 class HotelRecycler(private val context: Context, private val list:List<HotelDataClass>) :RecyclerView.Adapter<HotelRecycler.Viewholder>(){
     class Viewholder(itemview:View) :RecyclerView.ViewHolder(itemview) {
-
+        val title=itemview.findViewById<TextView>(R.id.title)
+        val address=itemview.findViewById<TextView>(R.id.addressdetail)
+        val price=itemview.findViewById<TextView>(R.id.price)
+        val pffpercent=itemview.findViewById<TextView>(R.id.off)
+        val rate=itemview.findViewById<TextView>(R.id.rates)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
-        TODO("Not yet implemented")
+        val view=LayoutInflater.from(context).inflate(R.layout.hotel,parent,false)
+        return Viewholder(view)
     }
 
     override fun getItemCount(): Int {
@@ -19,6 +27,6 @@ class HotelRecycler(private val context: Context, private val list:List<HotelDat
     }
 
     override fun onBindViewHolder(holder: Viewholder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 }
