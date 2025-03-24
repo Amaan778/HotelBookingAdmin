2239eb6bb331c72c211e82f4ca7340dc2f8df3a8
Amaan Ansari <amaanansari778@gmail.com>
2025-03-25 04:30:07 +0530

    Hotel Booking Admin

---
+++ app/src/main/java/com/app/hotelbookingadmin/login/LoginDetails.kt
@@ -0,0 +1,15 @@
+package com.app.hotelbookingadmin.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.hotelbookingadmin.R
+
+class LoginDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_details)
+    }
+}
