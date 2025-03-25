7a70bedcab6e2272253fbcdca4b0cfb949e643c9
Amaan Ansari <amaanansari778@gmail.com>
2025-03-26 04:37:57 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/addhotel/AddHotel.kt
+++ app/src/main/java/com/app/hotelbookingadmin/addhotel/AddHotel.kt
@@ -1,6 +1,8 @@
 package com.app.hotelbookingadmin.addhotel
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,10 @@ import androidx.core.view.WindowInsetsCompat
 import com.app.hotelbookingadmin.R
 
 class AddHotel : AppCompatActivity() {
+    private lateinit var coverimg:ImageView
+    private lateinit var coverbtn:Button
+    private lateinit var gallerybtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_hotel)
