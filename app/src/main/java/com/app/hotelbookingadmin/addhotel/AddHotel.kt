6ef18d5762ad82c16531173632f58a7817d2d589
Amaan Ansari <amaanansari778@gmail.com>
2025-03-29 18:41:33 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/addhotel/AddHotel.kt
+++ app/src/main/java/com/app/hotelbookingadmin/addhotel/AddHotel.kt
@@ -2,6 +2,7 @@ package com.app.hotelbookingadmin.addhotel
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +13,12 @@ import com.app.hotelbookingadmin.R
 class AddHotel : AppCompatActivity() {
     private lateinit var coverimg:ImageView
     private lateinit var coverbtn:Button
-    private lateinit var gallerybtn:Button
+    private lateinit var addhotelbtn:Button
+    private lateinit var title:EditText
+    private lateinit var address:EditText
+    private lateinit var rating:EditText
+    private lateinit var off:EditText
+    private lateinit var pricing:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,12 @@ class AddHotel : AppCompatActivity() {
 
         coverimg=findViewById(R.id.coverimg)
         coverbtn=findViewById(R.id.coverbtn)
-        gallerybtn=findViewById(R.id.gallerybtn)
+        addhotelbtn=findViewById(R.id.gallerybtn)
+        title=findViewById(R.id.title)
+        address=findViewById(R.id.address)
+        rating=findViewById(R.id.rating)
+        off=findViewById(R.id.off)
+        pricing=findViewById(R.id.pricing)
 
 
     }
