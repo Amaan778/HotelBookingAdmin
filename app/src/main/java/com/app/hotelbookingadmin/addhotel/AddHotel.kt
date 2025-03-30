1d34769c95875468599e733e27fc4f0ee53fbb40
Amaan Ansari <amaanansari778@gmail.com>
2025-03-30 22:43:35 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/addhotel/AddHotel.kt
+++ app/src/main/java/com/app/hotelbookingadmin/addhotel/AddHotel.kt
@@ -1,14 +1,21 @@
 package com.app.hotelbookingadmin.addhotel
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.app.hotelbookingadmin.R
+import com.yalantis.ucrop.UCrop
+import java.io.File
 
 class AddHotel : AppCompatActivity() {
     private lateinit var coverimg:ImageView
@@ -19,6 +26,11 @@ class AddHotel : AppCompatActivity() {
     private lateinit var rating:EditText
     private lateinit var off:EditText
     private lateinit var pricing:EditText
+
+    companion object {
+        private const val REQUEST_IMAGE_PICK = 100
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +46,34 @@ class AddHotel : AppCompatActivity() {
         pricing=findViewById(R.id.pricing)
 
         coverbtn.setOnClickListener {
-
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_IMAGE_PICK)
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+                val destinationUri = Uri.fromFile(File(cacheDir, "cropped_image.jpg"))
+
+                UCrop.of(uri, destinationUri)
+                    .withAspectRatio(1f, 1f) // Set aspect ratio (e.g., 1:1 for square crop)
+                    .withMaxResultSize(500, 500) // Set max resolution
+                    .start(this)
+            }
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == Activity.RESULT_OK) {
+            val resultUri = UCrop.getOutput(data!!)
+            resultUri?.let {
+                coverimg.setImageURI(it) // Set cropped image to ImageView
+            }
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val cropError = UCrop.getError(data!!)
+            Toast.makeText(this, "Crop Error: ${cropError?.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
 }
