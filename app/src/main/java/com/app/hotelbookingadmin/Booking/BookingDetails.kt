703212a0001df64acbcddfc1e99f1b8ca45a14f4
Amaan Ansari <amaanansari778@gmail.com>
2025-04-02 05:38:40 +0530

    Hotel Booking Admin

---
+++ app/src/main/java/com/app/hotelbookingadmin/Booking/BookingDetails.kt
@@ -0,0 +1,15 @@
+package com.app.hotelbookingadmin.Booking
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.hotelbookingadmin.R
+
+class BookingDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_details)
+    }
+}
