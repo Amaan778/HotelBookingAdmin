4a38a2cda8d8ee7ac7278ba228a495dfcb0c079a
Amaan Ansari <amaanansari778@gmail.com>
2025-04-18 04:26:23 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/login/LoginDetails.kt
+++ app/src/main/java/com/app/hotelbookingadmin/login/LoginDetails.kt
@@ -1,6 +1,7 @@
 package com.app.hotelbookingadmin.login
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,8 +9,15 @@ import androidx.core.view.WindowInsetsCompat
 import com.app.hotelbookingadmin.R
 
 class LoginDetails : AppCompatActivity() {
+    private lateinit var gmailtextview:TextView
+    private lateinit var passwordtextview:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_details)
+
+        gmailtextview=findViewById(R.id.gmailtextview)
+        passwordtextview=findViewById(R.id.passwordtextview)
+
     }
 }
