a3b6c3d6b087f95e48fc12770284b34f778560b3
Amaan Ansari <amaanansari778@gmail.com>
2025-04-22 04:18:50 +0530

    Hotel Booking Admin

--- app/src/main/java/com/app/hotelbookingadmin/Booking/BookingDetails.kt
+++ app/src/main/java/com/app/hotelbookingadmin/Booking/BookingDetails.kt
@@ -1,15 +1,25 @@
 package com.app.hotelbookingadmin.Booking
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.app.hotelbookingadmin.R
 
 class BookingDetails : AppCompatActivity() {
+    private lateinit var toolbar: TextView
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking_details)
+
+        toolbar=findViewById(R.id.toolbartextview)
+        recyclerView=findViewById(R.id.recycler)
+
     }
 }
